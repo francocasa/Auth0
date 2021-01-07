b67e676c6b17d2e1ea34c37a99079999f7c04cbf
francocasa <31408120+francocasa@users.noreply.github.com>
2021-01-07 01:24:41 -0500

    Auth0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProtegidaComponent } from './components/protegida/protegida.component';
+import { PreciosComponent } from './components/precios/precios.component';
+
+import { AuthModule } from '@auth0/auth0-angular';
+import { AuthButtonComponent } from './components/auth-button/auth-button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    ProtegidaComponent,
+    PreciosComponent,
+    AuthButtonComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, 
+    AuthModule.forRoot({
+      domain: 'dev-eyjjtzz2.us.auth0.com',
+      clientId: 'iu0uMwKeMYD9hu1LKEnDYF3HrsRJ4tud'
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
